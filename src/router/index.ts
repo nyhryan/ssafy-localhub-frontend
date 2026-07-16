415eddcb4c8705c96ba2b4307e04b4ba28812405
Byeolchan Kim <qufstar46@gmail.com>
2026-07-16 12:51:54 +0900

    feat: 브라우저 히스토리 이동시에 스크롤 위치 캐싱

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,53 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import ChatPage from '../pages/ChatPage.vue'
-import CurationPage from '../pages/CurationPage.vue'
-import HomePage from '../pages/HomePage.vue'
-import MapPage from '../pages/MapPage.vue'
-import NotFoundPage from '../pages/NotFoundPage.vue'
-import PostDetailPage from '../pages/PostDetailPage.vue'
-import PostFormPage from '../pages/PostFormPage.vue'
-import PostListPage from '../pages/PostListPage.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import ChatPage from "../pages/ChatPage.vue";
+import CurationPage from "../pages/CurationPage.vue";
+import HomePage from "../pages/HomePage.vue";
+import MapPage from "../pages/MapPage.vue";
+import NotFoundPage from "../pages/NotFoundPage.vue";
+import PostDetailPage from "../pages/PostDetailPage.vue";
+import PostFormPage from "../pages/PostFormPage.vue";
+import PostListPage from "../pages/PostListPage.vue";
 
 const router = createRouter({
   history: createWebHistory(),
-  scrollBehavior() {
-    return { top: 0 }
+  scrollBehavior(to, _from, savedPosition) {
+    if (savedPosition) {
+      return { ...savedPosition, behavior: "instant" };
+    }
+
+    if (to.hash) {
+      return { el: to.hash, behavior: "instant" };
+    }
+
+    return { top: 0, behavior: "instant" };
   },
   routes: [
-    { path: '/', name: 'home', component: HomePage },
-    { path: '/posts', name: 'posts', component: PostListPage },
-    { path: '/posts/new', name: 'post-create', component: PostFormPage },
-    { path: '/posts/:id', name: 'post-detail', component: PostDetailPage, props: true },
-    { path: '/posts/:id/edit', name: 'post-edit', component: PostFormPage, props: true },
-    { path: '/chat', name: 'chat', component: ChatPage },
-    { path: '/curation', name: 'curation', component: CurationPage },
-    { path: '/curation/all', name: 'curation-all', component: CurationPage, props: { mode: 'all' } },
-    { path: '/map', name: 'map', component: MapPage },
-    { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFoundPage },
+    { path: "/", name: "home", component: HomePage },
+    { path: "/posts", name: "posts", component: PostListPage },
+    { path: "/posts/new", name: "post-create", component: PostFormPage },
+    {
+      path: "/posts/:id",
+      name: "post-detail",
+      component: PostDetailPage,
+      props: true,
+    },
+    {
+      path: "/posts/:id/edit",
+      name: "post-edit",
+      component: PostFormPage,
+      props: true,
+    },
+    { path: "/chat", name: "chat", component: ChatPage },
+    { path: "/curation", name: "curation", component: CurationPage },
+    {
+      path: "/curation/all",
+      name: "curation-all",
+      component: CurationPage,
+      props: { mode: "all" },
+    },
+    { path: "/map", name: "map", component: MapPage },
+    { path: "/:pathMatch(.*)*", name: "not-found", component: NotFoundPage },
   ],
-})
+});
 
-export default router
+export default router;
